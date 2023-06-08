5f5fe848b30704ff84bdc8b979969876a7f86b63
Bal <afpsp6@neptun.gde.he>
2023-06-08 18:48:10 +0200

    Emberosztaly kesz

---
+++ src/model/Ember.java
@@ -0,0 +1,32 @@
+package model;
+
+public class Ember {
+
+    private String nev;
+    private int kor;
+
+    public Ember() {
+    }
+
+    public String getNev() {
+        return nev;
+    }
+
+    public void setNev(String nev) {
+        this.nev = nev;
+    }
+
+    public int getKor() {
+        return kor;
+    }
+
+    public void setKor(int kor) {
+        this.kor = kor;
+    }
+
+    public String toString() {
+        return "emberpéldány adatai: " + "\n nev:" + nev +
+                "\n kor:" + kor;
+    }
+
+}
